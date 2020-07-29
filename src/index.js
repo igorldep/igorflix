42f9e09522f185bba5106f2481b9a55d8b63eeee
Igor Luciano de Paula <igorldepaula@ibm.com>
2020-07-28 23:17:06 -0300

    Aula 2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/video';
+import CadastroCategoria from './pages/cadastro/categoria';
+
+const Pagina404 = () => (<h1 align='center'>Not Found - 404</h1>);
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  /*<React.StrictMode>
     <App />
   </React.StrictMode>,
+  
+      <Route component={App} />
+  */
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact />
+      <Route path="/home" component={App} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={() => (<h1 align='center'>Not Found - 404</h1>)} />
+      <Route component={Pagina404} />
+
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
